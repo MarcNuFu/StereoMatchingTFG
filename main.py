4ede2cae695ea72698475ca59c7cc46450db5e64
MarcNuFu <81378101+MarcNuFu@users.noreply.github.com>
2021-11-16 23:34:35 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import torch, torchvision
+from torchvision import datasets, transforms
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.autograd import Variable
+import dataloader.KITTIloader2015 as lt
+import dataloader.KITTILoader as DA
+
+def run_main():
+    batchsize = 1
+    num_epochs = 3
+    learnrate = 0.001
+    numWorkersTrain = 1
+    numWorkersTest = 1
+    betaOptim=(0.9, 0.999)
+    cudaAv = torch.cuda.is_available()
+
+    print(cudaAv)
+    return
+
+if __name__ == '__main__':
+    main()
